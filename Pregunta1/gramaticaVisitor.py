45317cceee3125b1c9b5a9f2852c231632103392
MelaniePS <melanie.perez@alumnos.uv.cl>
2023-09-28 23:36:42 -0400

    Update gramaticaVisitor.py

--- Pregunta1/gramaticaVisitor.py
+++ Pregunta1/gramaticaVisitor.py
@@ -53,6 +53,7 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#Mov2.
+    # esta funcion permite obtener el angulo y distancia que debe dibujar la tortuga
     def visitMov2(self, ctx:gramaticaParser.Mov2Context):
         angle = int(ctx.NUMBER(0).getText())
         move = int(ctx.NUMBER(1).getText())
