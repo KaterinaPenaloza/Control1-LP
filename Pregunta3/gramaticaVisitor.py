e2387e5c07925bd6e6683f8050c45b1eb7ebab05
Katerina Peñaloza <74738986+KaterinaPenaloza@users.noreply.github.com>
2023-09-28 23:43:07 -0300

    pregunta 3

--- Pregunta3/gramaticaVisitor.py
+++ Pregunta3/gramaticaVisitor.py
@@ -1,4 +1,4 @@
-# Generated from c:\\Users\\miap7\\Documents\\GitHub\\Control1-LP\\Pregunta3\\gramatica.g4 by ANTLR 4.9.2
+# Generated from c:\\Users\\miap7\\Documents\\GitHub\\Control1-LP\\Pregunta2\\gramatica.g4 by ANTLR 4.9.2
 from antlr4 import *
 import turtle
 from turtle import*
@@ -13,6 +13,7 @@ else:
 class gramaticaVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by gramaticaParser#prog.
+    #El inicio del programa
     def visitProg(self, ctx:gramaticaParser.ProgContext):
         return self.visitChildren(ctx)
 
@@ -23,11 +24,13 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#blank.
+    #Si el input está en blanco no hace nada
     def visitBlank(self, ctx:gramaticaParser.BlankContext):
         return self.visitChildren(ctx)
 
 
     # Visit a parse tree produced by gramaticaParser#On.
+    #Inicializa el puntero en forma de tortuga y "pone abajo el lapiz" para dibujar
     def visitOn(self, ctx:gramaticaParser.OnContext):
         turtle.showturtle()
         turtle.shape("turtle")
@@ -37,6 +40,7 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#Off.
+    #"Levanta el lapiz", de ese modo no dibuja.
     def visitOff(self, ctx:gramaticaParser.OffContext):
         turtle.up()
         #turtle.done()
@@ -45,6 +49,8 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#Rot.
+    # Rota el puntero a un cierto angulo dado por el usuario con la funcion turtle.right()
+    # esto es en sentido horario (turtle.right)
     def visitRot(self, ctx:gramaticaParser.RotContext):
         angle = int(ctx.NUMBER().getText())
         turtle.right(angle)
@@ -53,6 +59,7 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#Rot2.
+    # Rota el puntero a una coordenada dada.
     def visitRot2(self, ctx:gramaticaParser.Rot2Context):
         angle = int(ctx.NUMBER(0).getText()) # obtiene el angulo de rotacion
         angle2 = int(ctx.NUMBER(1).getText()) # obtiene el segundo angulo de rotacion 
@@ -61,20 +68,25 @@ class gramaticaVisitor(ParseTreeVisitor):
 
         return self.visitChildren(ctx)
     
-    # Visit a parse tree produced by gramaticaParser#Rot2.
+    # Visit a parse tree produced by gramaticaParser#Rot3.
+    # Se utiliza la base de la funcion mov2 que modifica la orientación del puntero y lo mueve hasta un punto dado
+    # por el usuario, luego cambia su orientacion como en la funcion Rot
     def visitRot3(self, ctx:gramaticaParser.Rot2Context):
+        #variables utilizadas para la funcion Mov2
         move = int(ctx.NUMBER(0).getText())
         move1 = int(ctx.NUMBER(1).getText())
+        #variables utilizadas para la funcion Rot
         angle = int(ctx.NUMBER(2).getText())
-        
+        #funcion para mover
         turtle.goto(move,move1)
+        #funcion para rotar
         turtle.right(angle)
-
 
         return self.visitChildren(ctx)
 
 
     # Visit a parse tree produced by gramaticaParser#Mov.
+    # El puntero se desplaza un numero dado por el usuario con la funcion turtle.forward()
     def visitMov(self, ctx:gramaticaParser.MovContext):
         move = int(ctx.NUMBER().getText())
         turtle.forward(move)
@@ -83,6 +95,8 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#Mov2.
+    # El puntero se cambia orientación en dirección al punto y luego
+    # se desplaza hasta el punto indicado por el usuario
     def visitMov2(self, ctx:gramaticaParser.Mov2Context):
         angle = int(ctx.NUMBER(0).getText())
         move = int(ctx.NUMBER(1).getText())
@@ -93,6 +107,7 @@ class gramaticaVisitor(ParseTreeVisitor):
 
 
     # Visit a parse tree produced by gramaticaParser#fin.
+    #Termina la ejecución utilizando la funcion turtle.done()
     def visitFin(self, ctx:gramaticaParser.FinContext):
         turtle.done()
         return self.visitChildren(ctx)
