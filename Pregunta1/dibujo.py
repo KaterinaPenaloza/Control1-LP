c4563f26ef6debd831261d374949a94461512011
Katerina Peñaloza <74738986+KaterinaPenaloza@users.noreply.github.com>
2023-11-11 21:33:22 -0300

    AST generado

--- Pregunta1/dibujo.py
+++ Pregunta1/dibujo.py
@@ -1,50 +1,44 @@
 import sys
 from antlr4 import *
 from antlr4.InputStream import InputStream
+from antlr4.tree.Tree import TerminalNode
 from gramaticaLexer import gramaticaLexer
 from gramaticaParser import gramaticaParser
 from gramaticaVisitor import gramaticaVisitor
 
-#Codigo base del programa para su funcionamiento
+# Función para imprimir un árbol sintáctico abstracto (AST) de manera más legible
+from antlr4.tree.Tree import TerminalNodeImpl
+
+def print_ast(node, indent=0):
+    if isinstance(node, TerminalNodeImpl):
+        # Imprimir solo el texto de los nodos terminales
+        print('  ' * indent + str(node.getText()))
+    else:
+        print('  ' * indent + str(type(node).__name__))
+        for child in node.getChildren():
+            print_ast(child, indent + 1)
+
 if __name__ == '__main__':
-    #Maneja los argumentos dados, si es un archivo o por input en la linea de comandos
+    # Maneja los argumentos dados, ya sea desde un archivo o desde la entrada estándar
     if len(sys.argv) > 1:
         input_stream = FileStream(sys.argv[1])
     else:
         input_stream = InputStream(sys.stdin.readline())
 
-#Definiciones de los archivos (lexer, los tokens a partir del analizador léxico y el parser)
+    # Definiciones de los archivos (lexer, los tokens a partir del analizador léxico y el parser)
     lexer = gramaticaLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = gramaticaParser(token_stream)
-#Se realiza el análisis sintáctico con el parser y se obtiene el AST
-#Luego el arbol generado se pasa a string en formato lisp
+
+    # Se realiza el análisis sintáctico con el parser y se obtiene el AST
     tree = parser.prog()
-    lisp_tree_str = tree.toStringTree(recog=parser)
-    lisp_tree_list = list(lisp_tree_str)
-    for x in range (len(lisp_tree_list)):
-        nivel = 0
-        for i in lisp_tree_list:
-            if(i == "("):
-                indexi = lisp_tree_list.index(i)
-                lisp_tree_list[indexi] = "\n"
 
-                nivel = nivel + 1
-                for t in range(nivel):
-                    indexi = lisp_tree_list.index(i)+t
-                    lisp_tree_list[indexi+t] = "\t"
-                break
+    # Imprime el árbol sintáctico abstracto (AST)
+    print("Abstract Syntax Tree (AST):")
+    print_ast(tree)
 
-        for j in reversed(lisp_tree_list):
-            if(j == ")"):
-                indexj = lisp_tree_list.index(j)
-                lisp_tree_list[indexj] = ""
-                nivel = nivel - 1 
-                break
-    
-
-    cadena_resultante = ''.join(lisp_tree_list)
-    print(cadena_resultante)
-#Se realiza la visita de los nodos para la ejecucion
+    # Se realiza la visita de los nodos para la ejecución
     visitor = gramaticaVisitor()
     visitor.visit(tree)
+
+    print("\nResultado de la ejecución:")
